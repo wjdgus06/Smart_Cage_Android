1f9d877fe700ca09050923eea88cccd0727964e1
stella <stella2837@naver.com>
2022-12-03 20:54:08 +0900

    make ManCageUpdateActivity.java

---
+++ app/src/main/java/com/example/reptile/ManCageUpdateActivity.java
@@ -0,0 +1,14 @@
+package com.example.reptile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManCageUpdateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_man_cage_update);
+    }
+}
