96ec6e69d9a377c68f644443efe4ff0f8e022315
lana <artist158@naver.com>
2022-12-03 18:54:06 +0900

    make naviBottom and MainActivity

--- app/src/main/java/com/example/reptile/LoginActivity.java
+++ app/src/main/java/com/example/reptile/LoginActivity.java
@@ -3,9 +3,9 @@ package com.example.reptile;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.reptile.databinding.ActivityLoginBinding;
 
-import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
@@ -24,35 +24,46 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         viewBinding = ActivityLoginBinding.inflate(getLayoutInflater());
 
-        viewBinding.btnRegister.setOnClickListener(view -> {
-            // 회원가입 액티비티 생성 후 연결
+        viewBinding.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userID = viewBinding.etId.getText().toString();
+                String userPW = viewBinding.etPw.getText().toString();
+
+                Response.Listener<String> response_listener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+                            if (success) {
+                                String userID = jsonObject.getString("userID");
+                                String userPW = jsonObject.getString("userPassword");
+                                Toast.makeText(getApplicationContext(), "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show();
+                                //출발 -> 도착
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                intent.putExtra("userID", userID);
+                                intent.putExtra("userPassword", userPW);
+                                startActivity(intent);
+                            }else {
+                                Toast.makeText(getApplicationContext(), "로그인에 실패하였습니다.", Toast.LENGTH_SHORT).show();
+                                return; //실패이므로 MainActivity로 넘어가면 X
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                LoginRequest loginRequest = new LoginRequest(userID, userPW, response_listener);
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);
+
+            }
         });
 
-        viewBinding.btnLogin.setOnClickListener(view -> {
-            Response.Listener<String> response_listener = response -> {
-                try {
-                    JSONObject jsonObject = new JSONObject(response);
-                    boolean success = jsonObject.getBoolean("success");
-                    if (success) {
-                        String userID = jsonObject.getString("userID");
-                        String userPW = jsonObject.getString("userPassword");
-                        Toast.makeText(getApplicationContext(), "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show();
-                        //출발 -> 도착
-                        Intent intent = new Intent(this, MainActivity.class);
-                        intent.putExtra("userID", userID);
-                        intent.putExtra("userPassword", userPW);
-                        startActivity(intent);
-                    }else {
-                        Toast.makeText(getApplicationContext(), "로그인에 실패하였습니다.", Toast.LENGTH_SHORT).show();
-                        return; //실패이므로 MainActivity 넘어가면 X
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            };
-            LoginRequest loginRequest = new LoginRequest(viewBinding.etId.getText().toString(), viewBinding.etPw.getText().toString(), response_listener);
-            RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
-            queue.add(loginRequest);
+        viewBinding.btnRegister.setOnClickListener(view -> {
+            // 회원가입 액티비티 생성 후 연결
+            Toast.makeText(getApplicationContext(), "Test btn_register", Toast.LENGTH_SHORT).show();
         });
 
         setContentView(viewBinding.getRoot());
--- app/src/main/java/com/example/reptile/MainActivity.java
+++ app/src/main/java/com/example/reptile/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.reptile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.reptile.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding viewBinding;
+    private final FragmentManager fragmentManager = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        viewBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(viewBinding.getRoot());
+
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(viewBinding.containerFragment.getId(), new HomeFragment()).commitAllowingStateLoss();
+
+        viewBinding.navBottom.setOnItemSelectedListener(new ItemSelectedListener());
+    }
+
+    class ItemSelectedListener implements NavigationBarView.OnItemSelectedListener {
+        @SuppressLint("NonConstantResourceId")
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+            switch (menuItem.getItemId()) {
+                case R.id.menu_home:
+                    transaction.replace(viewBinding.containerFragment.getId(), new HomeFragment()).commitAllowingStateLoss();
+                    break;
+                case R.id.menu_recent:
+                    transaction.replace(viewBinding.containerFragment.getId(), new ListFragment()).commitAllowingStateLoss();
+                    break;
+                case R.id.menu_search:
+                    transaction.replace(viewBinding.containerFragment.getId(), new NotificationFragment()).commitAllowingStateLoss();
+                    break;
+                case R.id.menu_setting:
+                    transaction.replace(viewBinding.containerFragment.getId(), new SettingFragment()).commitAllowingStateLoss();
+                    break;
+
+            }
+            return true;
+        }
     }
 }
