387566a9b7d0eb0aa9c5f23ea08e7c9d36498c9b
JeongHY-2001 <63927229+JeongHY-2001@users.noreply.github.com>
2022-12-04 00:40:49 +0900

    NewGroup

---
+++ app/src/main/java/com/example/reptile/NewGroupActivity.java
@@ -0,0 +1,14 @@
+package com.example.reptile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewGroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_group);
+    }
+}
