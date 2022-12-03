e7481d73dd0845678f8d89f5581b8dc6f4860cf0
lana <artist158@naver.com>
2022-12-04 04:07:54 +0900

    get type and cage list

--- app/src/main/java/com/example/reptile/VerTypeRVAdapter.java
+++ app/src/main/java/com/example/reptile/VerTypeRVAdapter.java
@@ -1,9 +1,11 @@
 package com.example.reptile;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -26,22 +28,25 @@ public class VerTypeRVAdapter extends RecyclerView.Adapter<VerTypeRVAdapter.View
         protected RecyclerView recyclerView;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            this.recyclerView = itemView.findViewById(R.id.ver_list_rv);
+            this.textView = itemView.findViewById(R.id.text_type_name);
+            this.recyclerView = itemView.findViewById(R.id.hor_list_rv);
         }
     }
 
     @NonNull
     @Override
     public VerTypeRVAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_home, null);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.design_list_type, parent, false);
         return new VerTypeRVAdapter.ViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull VerTypeRVAdapter.ViewHolder holder, int position) {
         HorCageRVAdapter hRVAdapter = new HorCageRVAdapter(typeList.get(position).getReptileList());
+
         holder.recyclerView.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL,false));
         holder.recyclerView.setAdapter(hRVAdapter);
+        holder.textView.setText(typeList.get(position).getTypeName());
     }
 
     @Override
