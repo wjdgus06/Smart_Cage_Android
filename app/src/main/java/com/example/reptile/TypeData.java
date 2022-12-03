a41fc5e988a8534fea16120c5fce48931e642242
lana <artist158@naver.com>
2022-12-04 00:01:23 +0900

    cagelist data, cagelist recycler adapter

---
+++ app/src/main/java/com/example/reptile/TypeData.java
@@ -0,0 +1,11 @@
+package com.example.reptile;
+
+public class TypeData {
+    private String typeName;
+    private ReptileData reptileData;
+
+    public TypeData(String name){
+        this.typeName = name;
+    }
+
+}
