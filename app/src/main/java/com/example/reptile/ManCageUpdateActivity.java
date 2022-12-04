064d40e14df5f4f99fb07adb5d93a9122058bc81
JeongHY-2001 <63927229+JeongHY-2001@users.noreply.github.com>
2022-12-05 02:31:54 +0900

    Create_CON

--- app/src/main/java/com/example/reptile/ManCageUpdateActivity.java
+++ app/src/main/java/com/example/reptile/ManCageUpdateActivity.java
@@ -1,5 +1,7 @@
 package com.example.reptile;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -7,23 +9,34 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.reptile.databinding.ActivityManCageUpdateBinding;
 
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONStringer;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class ManCageUpdateActivity extends AppCompatActivity {
 
     ActivityManCageUpdateBinding viewBinding;
+    Button Registerbutton;
+    EditText editname;
+    String aename;
+    String aedata;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +54,36 @@ public class ManCageUpdateActivity extends AppCompatActivity {
 
         System.out.println(grp_name);
 
-        getAEname(grp_name);
+        editname = findViewById(R.id.editText);
+        aename = editname.getText().toString(); // ae이름
+
+        Registerbutton = (Button) findViewById(R.id.button4);
+        Registerbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                aedata = getAEname(grp_name); //ae 이름 조회
+                if(aedata.contains(aename))
+                {
+                    //입력한 ae와 동일한 이름을 가진 디바이스 존재
+                    // con 등록
+                    String JsonMsg = makeJsonMsg();
+                    SendJsonToServer(JsonMsg, aename, "temperature");
+                    SendJsonToServer(JsonMsg, aename, "humidity");
+                    SendJsonToServer(JsonMsg, aename, "brightness");
+                }
+                else
+                {
+                    Toast myToast = Toast.makeText(getApplicationContext(),
+                            "ae 이름을 확인하세요!", Toast.LENGTH_SHORT);
+                    myToast.show();
+                }
+
+
+
+            }
+        });
+
+        getAEname(grp_name); //ae 이름 조회
 
         viewBinding = ActivityManCageUpdateBinding.inflate(getLayoutInflater());
         setContentView(viewBinding.getRoot());
@@ -119,5 +161,102 @@ public class ManCageUpdateActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         return mid;
+    }
+
+    private static String makeJsonMsg(){
+        String retMsg = "";
+
+        JSONStringer jsonStringer = new JSONStringer();
+
+        try{
+            retMsg = jsonStringer.object()
+                    .key("m2m:cnt").object()
+                    .key("rn").value("control")
+                    .key("lbl").array()
+                    .endArray()
+                    .key("mbs").value(16384)
+                    .endObject()
+                    .endObject().toString();
+            System.out.println(retMsg);
+
+            Log.d(TAG,"send string = " + retMsg);
+        }
+        catch(JSONException e)
+        {
+            e.printStackTrace();
+        }
+        return retMsg;
+    }
+
+    static public String SendJsonToServer(String JsonMsg, String ae_name, String sensor){
+        OutputStream os = null;
+        InputStream is = null;
+
+        int responseCode;
+
+        HttpURLConnection connection = null;
+
+        String response="";
+        String queryUrl = "http://182.221.64.162:7579/Mobius/"+ae_name+"/"+sensor;
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        try{
+            URL url = new URL(queryUrl);
+            connection = (HttpURLConnection) url.openConnection();
+
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Accept", "application/json");
+            connection.setRequestProperty("X-M2M-RI", "dks");
+            connection.setRequestProperty("X-M2M-Origin", "dks");
+            connection.setRequestProperty("Content-Type", "application/vnd.onem2m-res+json; ty=9");
+
+            // OutputStream으로 Post데이터 넘겨주는 옵션
+            connection.setDoOutput(true);
+
+            // InputStream으로 서버 응답 받는 옵션
+            connection.setDoInput(true);
+
+            os = connection.getOutputStream();
+            os.write(JsonMsg.getBytes()); //json넘겨주는 부분
+            os.flush();
+
+            responseCode = connection.getResponseCode();
+
+            System.out.println(responseCode);
+
+
+            // inputStream으로 응답
+            // 요 부분 잘 안됨....
+            if(responseCode==201){
+                System.out.println("HTTP_OK");
+                is = connection.getInputStream();
+                // inputStream to String
+                if(is != null)
+                {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is));
+                    StringBuffer stringBuffer = new StringBuffer();
+                    String inputLine;
+
+                    while ((inputLine = bufferedReader.readLine()) != null)  {
+                        stringBuffer.append(inputLine);
+                    }
+                    bufferedReader.close();
+
+                    response = stringBuffer.toString();
+                }
+                else
+                    response = "Error";
+                System.out.println(response);
+            }
+            else{
+                response = "Connection_Error";
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return response;
     }
 }
