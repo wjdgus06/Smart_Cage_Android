f3e05117e46e2801fa231902e0230121c0312afc
stella <stella2837@naver.com>
2022-12-08 14:59:13 +0900

    grpData

--- app/src/main/java/com/example/reptile/GrpData.java
+++ app/src/main/java/com/example/reptile/GrpData.java
@@ -8,11 +8,7 @@ public class GrpData {
     private static ArrayList<String> typeList = new ArrayList<String>(){{
         add("lizard");
         add("snake");
-<<<<<<< HEAD
         add("turtle");
-=======
-        add("testcameragrp2");
->>>>>>> f123739062f2276599b046419fa124184c0b7705
     }};
 
     public GrpData(){
